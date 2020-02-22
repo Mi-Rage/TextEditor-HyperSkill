74a48c6b71778840f9015f601a21d809151db4ee
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2020-02-22 19:46:55 +0300

    Этап 3/4 выполнен.
    Этап 3/4 выполнен.
    Добавлено меню File, позволяющее загрузить и сохранить файлы. Имя файла, исходя из задания, пока указывается в выделенном текстовом поле.
    Тест 3/4 HyperSkill пройден.

--- TextEditor.java
+++ TextEditor.java
@@ -2,6 +2,7 @@ package editor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,16 +10,18 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 
-public class TextEditor extends JFrame
-{
+public class TextEditor extends JFrame {
     final int WIDTH = 600;
     final int HEIGHT = 400;
-    JTextArea textArea;
-    Container container;
+    private JTextArea textArea;         //основное текстовое поле
+    private Container container;        //основной контейнер
+    private JButton saveButton;         //кнопка SAVE
+    private JButton loadButton;         //кнопка LOAD
+    private JTextField filenameField;   //Поле ввода имени файла
 
-    public TextEditor()
-    {
-        super("Text Editor v.2.1");
+
+    public TextEditor() {
+        super("Text Editor v.3.1");
 
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -38,48 +41,33 @@ public class TextEditor extends JFrame
         scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
         //Собираем все панели для вывода на экран
+
         container.add(scrollableTextArea, BorderLayout.CENTER);
         container.add(upArea(), BorderLayout.NORTH);
         container.add(new JLabel(" "), BorderLayout.SOUTH);    //Просто для красоты
         container.add(new JLabel("    "), BorderLayout.WEST);  //Просто для красоты
         container.add(new JLabel("    "), BorderLayout.EAST);  //Просто для красоты
-        // Выводим окно на экран
-        setVisible(true);
+
+        createListener();          //Установим слушатели
+        menuBar();                 //Добавляем строку меню
+
+        setVisible(true);          // Выводим окно на экран
     }
 
     //Верхняя область с полем имени файла и кнопками SAVE и LOAD
-    private JPanel upArea(){
+    private JPanel upArea() {
         JPanel upArea = new JPanel();
         upArea.setLayout(new FlowLayout(FlowLayout.CENTER));
-        JTextField filenameField = new JTextField(30);
+        filenameField = new JTextField(30);
         filenameField.setName("FilenameField");
 
-        //Сохраняем в файл
-        JButton saveButton = new JButton("Save");
+        //Создаем кнопку SAVE. Слушатель в отдельном методе.
+        saveButton = new JButton("Save");
         saveButton.setName("SaveButton");
-        saveButton.addActionListener(actionEvent -> {
-            File file = new File(filenameField.getText());
 
-            try (FileWriter writer = new FileWriter(file)) {
-                writer.write(textArea.getText());
-            } catch (IOException e) {
-                JOptionPane.showMessageDialog(container,
-                    "ERROR!\nНевозможно создать файл:\n " + filenameField.getText());
-            }
-        });
-        //Загрузка файла с именем в текстовом поле
-        JButton loadButton = new JButton("Load");
+        //Создаем кнопку LOAD. Слушатель в отдельном методе.
+        loadButton = new JButton("Load");
         loadButton.setName("LoadButton");
-        loadButton.addActionListener(actionEvent -> {
-            try{
-               textArea.setText(new String(Files.readAllBytes(Paths.get(filenameField.getText()))));
-            } catch (IOException e) {
-//                Если открыть - будет классное окно с указанием ошибки. Но тест им пользоваться не умеет.
-//                JOptionPane.showMessageDialog(container,
-//                        "ERROR!\nНе найден файл:\n" + filenameField.getText());
-                textArea.setText(null);
-            }
-        });
 
         // Собираем панель из поля ввода имени и 2х кнопок
         upArea.add(filenameField);
@@ -89,7 +77,69 @@ public class TextEditor extends JFrame
         return upArea;
     }
 
+    public void menuBar() {
+        //Создаем строку меню
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
 
+        //Создаем меню FILE
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setName("MenuFile");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(fileMenu);
+
+        //Создаем пункт LOAD с мнемокомандой и возьмем слушатель от кнопки LOAD
+        JMenuItem loadMenuItem = new JMenuItem("Load");
+        loadMenuItem.setName("MenuLoad");
+        loadMenuItem.addActionListener(loadButton.getActionListeners()[0]);
+        loadMenuItem.setMnemonic(KeyEvent.VK_L);
+
+
+        //Создаем пункт SAVE с мнемокомандой и возьмем слушатель от кнопки SAVE
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.setName("MenuSave");
+        saveMenuItem.setMnemonic(KeyEvent.VK_S);
+        saveMenuItem.addActionListener(saveButton.getActionListeners()[0]);
+
+        //Создаем пункт EXIT с мнемокодом и безопасным выходом
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.setName("MenuExit");
+        exitMenuItem.setMnemonic(KeyEvent.VK_E);
+        exitMenuItem.addActionListener(actionEvent -> dispose());
+
+        //Добавим пункты в меню
+        fileMenu.add(loadMenuItem);
+        fileMenu.add(saveMenuItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenuItem);
+    }
+
+    //Устанавливаем все слушатели в одном месте
+    public void createListener(){
+
+        //Для операций SAVE
+        saveButton.addActionListener(actionEvent -> {
+            File file = new File(filenameField.getText());
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write(textArea.getText());
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(container,
+                        "ERROR!\nНевозможно создать файл:\n " + filenameField.getText());
+            }
+        });
+
+        //Для операций LOAD
+        loadButton.addActionListener(actionEvent -> {
+            try {
+                textArea.setText(new String(Files.readAllBytes(Paths.get(filenameField.getText()))));
+            } catch (IOException e) {
+//                Если открыть - будет классное окно с указанием ошибки. Но тест им пользоваться не умеет.
+//                JOptionPane.showMessageDialog(container,
+//                        "ERROR!\nНе найден файл:\n" + filenameField.getText());
+                textArea.setText(null);
+            }
+        });
+    }
 }
 
 
